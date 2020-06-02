cedd2815d3851ddefd00fc0768ed13e7e10f8575
费陶勇 <2460392754@qq.com>
2020-06-02 20:02:26 +0800

    refactor: 删除多余的代码

--- App.vue
+++ App.vue
@@ -1,17 +1,3 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {};
 </script>
-
-<style>
-	/*每个页面公共css */
-</style>
